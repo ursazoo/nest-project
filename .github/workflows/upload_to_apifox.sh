d878744a21cc40619c87873a542d1e619c5de5af
caoqinfeng <caoqinfeng@fshows.com>
2024-09-06 17:42:21 +0800

    chore: 传入环境变量

--- .github/workflows/upload_to_apifox.sh
+++ .github/workflows/upload_to_apifox.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# 打印环境变量，确保它们被正确设置
-echo "APIFOX_PROJECT_ID: $APIFOX_PROJECT_ID"
-echo "APIFOX_API_TOKEN: $APIFOX_API_TOKEN"
+# 确保脚本可以正确运行
+set -e
 
-# 变量设置
-# APIFOX_PROJECT_ID="5118368"       # 替换为你的 Apifox 项目 ID
-# APIFOX_API_TOKEN="your_api_token"         # 替换为你的 Apifox API Token
-SWAGGER_FILE_PATH="./swagger.json"        # Swagger JSON 文件的路径
+# 打印环境变量，帮助调试
+echo "Uploading Swagger to Apifox..."
+echo "APIFOX_PROJECT_ID === : $APIFOX_PROJECT_ID"
+echo "APIFOX_API_TOKEN === : $APIFOX_API_TOKEN"
 
-curl --location -g --request POST "https://api.apifox.com/v1/projects/$APIFOX_PROJECT_ID/import-openapi?locale=zh-CN" \
---header 'X-Apifox-Api-Version;' \
---header "Authorization: Bearer $APIFOX_API_TOKEN" \
---header 'User-Agent: Apifox/1.0.0 (https://apifox.com)' \
---header 'Content-Type: application/json' \
+# 使用环境变量替换占位符并上传 Swagger 文档
+curl --location --request POST "https://api.apifox.com/v1/projects/${APIFOX_PROJECT_ID}/import-openapi?locale=zh-CN" \
+--header "X-Apifox-Api-Version: 1" \
+--header "Authorization: Bearer ${APIFOX_API_TOKEN}" \
+--header "User-Agent: Apifox/1.0.0 (https://apifox.com)" \
+--header "Content-Type: application/json" \
 --data-raw '{
     "input": "string",
     "options": {
@@ -25,3 +25,5 @@ curl --location -g --request POST "https://api.apifox.com/v1/projects/$APIFOX_PR
         "prependBasePath": false
     }
 }'
+
+echo "Upload complete."
